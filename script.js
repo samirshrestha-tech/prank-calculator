7328a78d76944cf19667af991c6bafc3e906e4dd
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2023-09-05 15:59:54 +1000

    prank calculator

---
+++ script.js
@@ -0,0 +1,33 @@
+const allbtns = [...document.getElementsByClassName("btn")];
+// console.log(allbtns);
+let strToDisplay = "";
+const displaElm = document.querySelector(".display");
+const 
+
+allbtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // console.log(allbtns);
+    const val = btn.innerText;
+
+    if (val === "AC") {
+      strToDisplay = "";
+      display(strToDisplay);
+    }
+    if (val === "C") {
+      const str = strToDisplay.slice(0, -1);
+      return display(strToDisplay);
+    }
+    if (val=== "="){
+        return total();
+    }
+    strToDisplay += val;
+    display(strToDisplay);
+  });
+});
+
+const display = (str) => {
+  displaElm.innerText = str || "0.00";
+};
+const total =(ttl)=>{
+    return eval(strToDisplay)
+}
